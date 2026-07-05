c0ddc2d74ce60f584b175919fe5fb16db595ab8c
Lavanya Sinha <lavanyasinha50@gmail.com>
2026-07-05 19:15:27 +0530

    Time: 1 ms (89.59%), Space: 42.4 MB (85.36%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        if(num<= 0){
+            return false;
+        }
+        while(num % 4 == 0){
+            num = num / 4;
+        }
+        if(num == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}

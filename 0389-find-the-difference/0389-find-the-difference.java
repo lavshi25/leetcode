e3e4ddfeb45e84a8784abb5bb833c129d1b6d75d
Lavanya Sinha <lavanyasinha50@gmail.com>
2026-08-23 19:44:57 +0530

    Time: 12 ms (11.01%), Space: 43.4 MB (20.23%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -4,7 +4,7 @@ class Solution {
         for(char c: s.toCharArray()){
             count1.put(c , count1.getOrDefault(c,0)+1);
         }
-        HashMap<Character,Integer> count2 = new HashMap<>();
+       
         for(char c: t.toCharArray()){
             if(count1.containsKey(c) && count1.get(c)>0){
                 count1.put(c, count1.get(c)-1);
